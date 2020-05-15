19e44e249be05040fbf84e8022e95ec2504b5d91
tatsumi mukai <63081975+pxiqlops@users.noreply.github.com>
2020-05-15 17:28:49 +0900

    電卓アプリ

---
+++ 20200417/Form1.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class keisan
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.num7 = New System.Windows.Forms.Button()
+        Me.num2 = New System.Windows.Forms.Button()
+        Me.num1 = New System.Windows.Forms.Button()
+        Me.num3 = New System.Windows.Forms.Button()
+        Me.num5 = New System.Windows.Forms.Button()
+        Me.num6 = New System.Windows.Forms.Button()
+        Me.num9 = New System.Windows.Forms.Button()
+        Me.num8 = New System.Windows.Forms.Button()
+        Me.num0 = New System.Windows.Forms.Button()
+        Me.num4 = New System.Windows.Forms.Button()
+        Me.divibt = New System.Windows.Forms.Button()
+        Me.multibt = New System.Windows.Forms.Button()
+        Me.minbt = New System.Windows.Forms.Button()
+        Me.plusbt = New System.Windows.Forms.Button()
+        Me.ansbt = New System.Windows.Forms.Button()
+        Me.pribt = New System.Windows.Forms.Button()
+        Me.deletebt = New System.Windows.Forms.Button()
+        Me.answer20 = New System.Windows.Forms.Label()
+        Me.answer2 = New System.Windows.Forms.TextBox()
+        Me.answer1 = New System.Windows.Forms.TextBox()
+        Me.answer3 = New System.Windows.Forms.TextBox()
+        Me.kigo = New System.Windows.Forms.TextBox()
+        Me.resultbox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.resultreset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'num7
+        '
+        Me.num7.Location = New System.Drawing.Point(70, 135)
+        Me.num7.Name = "num7"
+        Me.num7.Size = New System.Drawing.Size(50, 50)
+        Me.num7.TabIndex = 0
+        Me.num7.Text = "7"
+        Me.num7.UseVisualStyleBackColor = True
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(172, 317)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(50, 50)
+        Me.num2.TabIndex = 2
+        Me.num2.Text = "2"
+        Me.num2.UseVisualStyleBackColor = True
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(70, 317)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(50, 50)
+        Me.num1.TabIndex = 3
+        Me.num1.Text = "1"
+        Me.num1.UseVisualStyleBackColor = True
+        '
+        'num3
+        '
+        Me.num3.Location = New System.Drawing.Point(270, 317)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(50, 50)
+        Me.num3.TabIndex = 4
+        Me.num3.Text = "3"
+        Me.num3.UseVisualStyleBackColor = True
+        '
+        'num5
+        '
+        Me.num5.Location = New System.Drawing.Point(172, 226)
+        Me.num5.Name = "num5"
+        Me.num5.Size = New System.Drawing.Size(50, 50)
+        Me.num5.TabIndex = 5
+        Me.num5.Text = "5"
+        Me.num5.UseVisualStyleBackColor = True
+        '
+        'num6
+        '
+        Me.num6.Location = New System.Drawing.Point(270, 226)
+        Me.num6.Name = "num6"
+        Me.num6.Size = New System.Drawing.Size(50, 50)
+        Me.num6.TabIndex = 6
+        Me.num6.Text = "6"
+        Me.num6.UseVisualStyleBackColor = True
+        '
+        'num9
+        '
+        Me.num9.Location = New System.Drawing.Point(270, 135)
+        Me.num9.Name = "num9"
+        Me.num9.Size = New System.Drawing.Size(50, 50)
+        Me.num9.TabIndex = 7
+        Me.num9.Text = "9"
+        Me.num9.UseVisualStyleBackColor = True
+        '
+        'num8
+        '
+        Me.num8.Location = New System.Drawing.Point(172, 135)
+        Me.num8.Name = "num8"
+        Me.num8.Size = New System.Drawing.Size(50, 50)
+        Me.num8.TabIndex = 8
+        Me.num8.Text = "8"
+        Me.num8.UseVisualStyleBackColor = True
+        '
+        'num0
+        '
+        Me.num0.Location = New System.Drawing.Point(172, 408)
+        Me.num0.Name = "num0"
+        Me.num0.Size = New System.Drawing.Size(50, 50)
+        Me.num0.TabIndex = 9
+        Me.num0.Text = "0"
+        Me.num0.UseVisualStyleBackColor = True
+        '
+        'num4
+        '
+        Me.num4.Location = New System.Drawing.Point(70, 226)
+        Me.num4.Name = "num4"
+        Me.num4.Size = New System.Drawing.Size(50, 50)
+        Me.num4.TabIndex = 10
+        Me.num4.Text = "4"
+        Me.num4.UseVisualStyleBackColor = True
+        '
+        'divibt
+        '
+        Me.divibt.Location = New System.Drawing.Point(365, 135)
+        Me.divibt.Name = "divibt"
+        Me.divibt.Size = New System.Drawing.Size(50, 50)
+        Me.divibt.TabIndex = 14
+        Me.divibt.Text = "/"
+        Me.divibt.UseVisualStyleBackColor = True
+        '
+        'multibt
+        '
+        Me.multibt.Location = New System.Drawing.Point(365, 226)
+        Me.multibt.Name = "multibt"
+        Me.multibt.Size = New System.Drawing.Size(50, 50)
+        Me.multibt.TabIndex = 13
+        Me.multibt.Text = "*"
+        Me.multibt.UseVisualStyleBackColor = True
+        '
+        'minbt
+        '
+        Me.minbt.Location = New System.Drawing.Point(365, 317)
+        Me.minbt.Name = "minbt"
+        Me.minbt.Size = New System.Drawing.Size(50, 50)
+        Me.minbt.TabIndex = 12
+        Me.minbt.Text = "-"
+        Me.minbt.UseVisualStyleBackColor = True
+        '
+        'plusbt
+        '
+        Me.plusbt.Location = New System.Drawing.Point(365, 408)
+        Me.plusbt.Name = "plusbt"
+        Me.plusbt.Size = New System.Drawing.Size(50, 50)
+        Me.plusbt.TabIndex = 15
+        Me.plusbt.Text = "+"
+        Me.plusbt.UseVisualStyleBackColor = True
+        '
+        'ansbt
+        '
+        Me.ansbt.Location = New System.Drawing.Point(270, 408)
+        Me.ansbt.Name = "ansbt"
+        Me.ansbt.Size = New System.Drawing.Size(50, 50)
+        Me.ansbt.TabIndex = 16
+        Me.ansbt.Text = "="
+        Me.ansbt.UseVisualStyleBackColor = True
+        '
+        'pribt
+        '
+        Me.pribt.Location = New System.Drawing.Point(70, 408)
+        Me.pribt.Name = "pribt"
+        Me.pribt.Size = New System.Drawing.Size(50, 50)
+        Me.pribt.TabIndex = 17
+        Me.pribt.Text = "."
+        Me.pribt.UseVisualStyleBackColor = True
+        '
+        'deletebt
+        '
+        Me.deletebt.Location = New System.Drawing.Point(365, 45)
+        Me.deletebt.Name = "deletebt"
+        Me.deletebt.Size = New System.Drawing.Size(50, 50)
+        Me.deletebt.TabIndex = 18
+        Me.deletebt.Text = "DEL"
+        Me.deletebt.UseVisualStyleBackColor = True
+        '
+        'answer20
+        '
+        Me.answer20.AutoSize = True
+        Me.answer20.BackColor = System.Drawing.SystemColors.Control
+        Me.answer20.Font = New System.Drawing.Font("ＭＳ ゴシック", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.answer20.Location = New System.Drawing.Point(67, 31)
+        Me.answer20.Name = "answer20"
+        Me.answer20.Size = New System.Drawing.Size(0, 17)
+        Me.answer20.TabIndex = 19
+        '
+        'answer2
+        '
+        Me.answer2.Location = New System.Drawing.Point(70, 45)
+        Me.answer2.MaxLength = 20
+        Me.answer2.Name = "answer2"
+        Me.answer2.Size = New System.Drawing.Size(250, 22)
+        Me.answer2.TabIndex = 22
+        '
+        'answer1
+        '
+        Me.answer1.Location = New System.Drawing.Point(70, 87)
+        Me.answer1.MaxLength = 8
+        Me.answer1.Name = "answer1"
+        Me.answer1.Size = New System.Drawing.Size(103, 22)
+        Me.answer1.TabIndex = 23
+        '
+        'answer3
+        '
+        Me.answer3.Location = New System.Drawing.Point(217, 87)
+        Me.answer3.MaxLength = 8
+        Me.answer3.Name = "answer3"
+        Me.answer3.Size = New System.Drawing.Size(103, 22)
+        Me.answer3.TabIndex = 24
+        '
+        'kigo
+        '
+        Me.kigo.Location = New System.Drawing.Point(179, 87)
+        Me.kigo.Name = "kigo"
+        Me.kigo.Size = New System.Drawing.Size(32, 22)
+        Me.kigo.TabIndex = 25
+        Me.kigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'resultbox
+        '
+        Me.resultbox.Location = New System.Drawing.Point(454, 71)
+        Me.resultbox.Multiline = True
+        Me.resultbox.Name = "resultbox"
+        Me.resultbox.Size = New System.Drawing.Size(355, 387)
+        Me.resultbox.TabIndex = 26
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(451, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 15)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "計算履歴"
+        '
+        'resultreset
+        '
+        Me.resultreset.Location = New System.Drawing.Point(730, 31)
+        Me.resultreset.Name = "resultreset"
+        Me.resultreset.Size = New System.Drawing.Size(79, 29)
+        Me.resultreset.TabIndex = 28
+        Me.resultreset.Text = "リセット"
+        Me.resultreset.UseVisualStyleBackColor = True
+        '
+        'keisan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(883, 512)
+        Me.Controls.Add(Me.resultreset)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.resultbox)
+        Me.Controls.Add(Me.kigo)
+        Me.Controls.Add(Me.answer3)
+        Me.Controls.Add(Me.answer1)
+        Me.Controls.Add(Me.answer2)
+        Me.Controls.Add(Me.answer20)
+        Me.Controls.Add(Me.deletebt)
+        Me.Controls.Add(Me.pribt)
+        Me.Controls.Add(Me.ansbt)
+        Me.Controls.Add(Me.plusbt)
+        Me.Controls.Add(Me.divibt)
+        Me.Controls.Add(Me.multibt)
+        Me.Controls.Add(Me.minbt)
+        Me.Controls.Add(Me.num4)
+        Me.Controls.Add(Me.num0)
+        Me.Controls.Add(Me.num8)
+        Me.Controls.Add(Me.num9)
+        Me.Controls.Add(Me.num6)
+        Me.Controls.Add(Me.num5)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.num7)
+        Me.Name = "keisan"
+        Me.Text = "電卓"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents num7 As Button
+    Friend WithEvents num2 As Button
+    Friend WithEvents num1 As Button
+    Friend WithEvents num3 As Button
+    Friend WithEvents num5 As Button
+    Friend WithEvents num6 As Button
+    Friend WithEvents num9 As Button
+    Friend WithEvents num8 As Button
+    Friend WithEvents num0 As Button
+    Friend WithEvents num4 As Button
+    Friend WithEvents divibt As Button
+    Friend WithEvents multibt As Button
+    Friend WithEvents minbt As Button
+    Friend WithEvents plusbt As Button
+    Friend WithEvents ansbt As Button
+    Friend WithEvents pribt As Button
+    Friend WithEvents deletebt As Button
+    Friend WithEvents answer20 As Label
+    Friend WithEvents answer2 As TextBox
+    Friend WithEvents answer1 As TextBox
+    Friend WithEvents answer3 As TextBox
+    Friend WithEvents kigo As TextBox
+    Friend WithEvents resultbox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents resultreset As Button
+End Class
---
+++ 20200417/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class keisan
+    Dim kigou As Integer = 0
+    Dim number1 As Decimal = 0
+    Dim number2 As Decimal = 0
+    Dim result As String = ""
+
+    Private Sub num0_Click(sender As Object, e As EventArgs) Handles num0.Click
+        If kigou = 0 Then
+            If answer1.Text = "" Then
+                answer1.Text = 0
+            Else
+                If answer1.Text <> 0 Then
+                    answer1.Text = answer1.Text & 0
+                End If
+            End If
+        Else
+            If answer3.Text = "" Then
+                answer1.Text = answer1.Text & 0
+                answer3.Text = 0
+            Else
+                If answer3.Text = 0 Then
+                    answer3.Text = 0
+                Else
+                    answer3.Text = answer3.Text & 0
+                End If
+            End If
+        End If
+    End Sub
+    Private Sub num1_Click(sender As Object, e As EventArgs) Handles num1.Click
+        push(1)
+    End Sub
+    Private Sub num2_Click(sender As Object, e As EventArgs) Handles num2.Click
+        push(2)
+    End Sub
+    Private Sub num3_Click(sender As Object, e As EventArgs) Handles num3.Click
+        push(3)
+    End Sub
+    Private Sub num4_Click(sender As Object, e As EventArgs) Handles num4.Click
+        push(4)
+    End Sub
+    Private Sub num5_Click(sender As Object, e As EventArgs) Handles num5.Click
+        push(5)
+    End Sub
+    Private Sub num6_Click(sender As Object, e As EventArgs) Handles num6.Click
+        push(6)
+    End Sub
+    Private Sub num7_Click(sender As Object, e As EventArgs) Handles num7.Click
+        push(7)
+    End Sub
+    Private Sub num8_Click(sender As Object, e As EventArgs) Handles num8.Click
+        push(8)
+    End Sub
+    Private Sub num9_Click(sender As Object, e As EventArgs) Handles num9.Click
+        push(9)
+    End Sub
+
+    Private Sub deletebt_Click(sender As Object, e As EventArgs) Handles deletebt.Click
+        answer1.Text = ""
+        answer2.Text = ""
+        answer3.Text = ""
+        kigo.Text = ""
+        kigou = 0
+        number1 = 0
+        number2 = 0
+    End Sub
+
+    Private Sub plusbt_Click(sender As Object, e As EventArgs) Handles plusbt.Click
+        If IsNumeric(answer1.Text) = True Then
+            number1 = answer1.Text
+            kigo.Text = "+"
+            kigou = 1
+        End If
+    End Sub
+
+    Private Sub minbt_Click(sender As Object, e As EventArgs) Handles minbt.Click
+        If IsNumeric(answer1.Text) = True Then
+            number1 = answer1.Text
+            kigo.Text = "-"
+            kigou = 2
+        End If
+    End Sub
+
+    Private Sub multibt_Click(sender As Object, e As EventArgs) Handles multibt.Click
+        If IsNumeric(answer1.Text) = True Then
+            number1 = answer1.Text
+            kigo.Text = "*"
+            kigou = 3
+        End If
+    End Sub
+
+    Private Sub divibt_Click(sender As Object, e As EventArgs) Handles divibt.Click
+        If IsNumeric(answer1.Text) = True Then
+            number1 = answer1.Text
+            kigo.Text = "/"
+            kigou = 4
+        End If
+    End Sub
+
+    Private Sub ansbt_Click(sender As Object, e As EventArgs) Handles ansbt.Click
+        If kigou = 0 Then
+            MessageBox.Show("数字が入力されておりません", "エラー", MessageBoxButtons.OK)
+        Else
+            number2 = answer3.Text
+            If kigou = 4 And number2 = 0 Then
+                MessageBox.Show("0で割ることは出来ません", "エラー", MessageBoxButtons.OK)
+            Else
+                Select Case kigou
+                    Case 1
+                        answer2.Text = number1 + number2
+                    Case 2
+                        answer2.Text = number1 - number2
+                    Case 3
+                        answer2.Text = number1 * number2
+                    Case 4
+                        answer2.Text = number1 / number2
+                End Select
+                resultbox.Text = number1 & kigo.Text & number2 & "=" & answer2.Text & vbCrLf & resultbox.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub pribt_Click(sender As Object, e As EventArgs) Handles pribt.Click
+        If answer1.Text.Contains(".") = False Then
+            If answer1.Text = "" Then
+                answer1.Text = answer1.Text & "0."
+            End If
+        End If
+    End Sub
+
+    Public Sub push(num As Integer)
+        If kigou = 0 Then
+            If answer1.Text = "" Then
+                answer1.Text = num
+            Else
+                If answer1.Text = 0 Then
+                    answer1.Text = num
+                Else
+                    answer1.Text = answer1.Text & num
+                End If
+            End If
+        Else
+            If answer3.Text = "" Then
+                answer3.Text = num
+            Else
+                If answer3.Text = 0 Then
+                    answer3.Text = num
+                Else
+                    answer3.Text = answer3.Text & num
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub resultreset_Click(sender As Object, e As EventArgs) Handles resultreset.Click
+        resultbox.Text = ""
+    End Sub
+End Class
